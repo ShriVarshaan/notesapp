6f0407d105b89ecf20b091ed7a9f6a432461397a
ShriVarshaan <varshaanvijay@gmail.com>
2026-02-22 22:08:33 +0000

    minor fixes

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import toast from "react-hot-toast"
 
 const API = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? import.meta.env.VITE_API_URL : "/api",
+    baseURL: import.meta.env.MODE === "development" ? import.meta.env.VITE_API_URL : "/",
     withCredentials: true,
 });
 
